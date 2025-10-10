c839049697d3c8b4a47476a32dd3f0c5af60736a
Ahmed Omran <mamdouhanahamada@gmail.com>
2025-10-10 17:36:46 +0300

    handle initaiol reoute if the user already logged in

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,5 +1,6 @@
-
 import 'package:flutter/material.dart';
+import 'package:state_managemnet_di_newtorking_task/core/constants/constants.dart';
+import 'package:state_managemnet_di_newtorking_task/core/services/prefs.dart';
 
 import 'core/routes/app_routes.dart';
 import 'core/routes/on_generate_routes.dart';
@@ -14,7 +15,15 @@ class MyApp extends StatelessWidget {
       theme: darkTheme,
       debugShowCheckedModeBanner: false,
       onGenerateRoute: onGenerateRoute,
-      initialRoute: AppRoutes.login,
+      initialRoute: _handleInitialRoute(),
     );
+  }
+
+  String _handleInitialRoute() {
+    final accessToken = Prefs.getString(kAccessToken);
+    if (accessToken != null && accessToken.isNotEmpty) {
+      return AppRoutes.home;
+    }
+    return AppRoutes.login;
   }
 }
